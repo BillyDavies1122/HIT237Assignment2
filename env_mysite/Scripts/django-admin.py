118e02b54146c06044b21d69fcb42261b4a9a52a
BillyDavies1122 <36976243+BillyDavies1122@users.noreply.github.com>
2018-05-12 14:51:55 +0930

    Project Started

---
+++ env_mysite/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\billy\documents\github\hit237assignment2\env_mysite\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
